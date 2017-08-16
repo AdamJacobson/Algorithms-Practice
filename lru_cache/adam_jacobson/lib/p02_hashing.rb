b2bc1b1c5b2316fbe9a62d68b6b2e21785bf0367
adam <amjacobson0@gmail.com>
2017-08-15 20:41:42 -0700

    Hash complete

--- lru_cache/adam_jacobson/lib/p02_hashing.rb
+++ lru_cache/adam_jacobson/lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    h = 0
+    self.each_with_index do |el, idx|
+      h = h ^ ((idx + 1) * el.hash)
+    end
+    h
   end
 end
 
 class String
   def hash
+    self.chars.map(&:ord).hash
   end
 end
 
@@ -16,6 +22,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.sort.hash
   end
 end
