fc9a1a4db18f16f1c57751245b99f5709210fc15
adam <amjacobson0@gmail.com>
2017-08-16 08:53:39 -0700

    Complete practical

--- lru_cache/adam_jacobson/lib/p07_practical.rb
+++ lru_cache/adam_jacobson/lib/p07_practical.rb
@@ -1,5 +1,23 @@
 require_relative 'p05_hash_map'
 
-def can_string_be_palindrome?(string) 
-  
+def can_string_be_palindrome?(string)
+  # examples
+  # ABA
+  # ABBA
+  # ABCBA
+
+  # even number: all characters must be even count
+  # odd number: one character must have odd count
+
+  count = Hash.new(0)
+  string.chars.each { |ch| count[ch] += 1 }
+  num_odd = 0
+
+  count.values.each { |val| num_odd += 1 if val.odd? }
+
+  if string.chars.count.even?
+    num_odd == 0
+  else
+    num_odd == 1
+  end
 end
