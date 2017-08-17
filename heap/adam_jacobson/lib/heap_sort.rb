e9c85d857f91093320a2ee6ccf21d1d5f92a4036
adam <amjacobson0@gmail.com>
2017-08-17 11:40:49 -0700

    Heapsort

--- heap/adam_jacobson/lib/heap_sort.rb
+++ heap/adam_jacobson/lib/heap_sort.rb
@@ -2,5 +2,17 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    self.length.times do |i|
+      BinaryMinHeap.heapify_up(self, i)
+    end
+
+    self.length.times do |i|
+      last = self.length - 1 - i
+
+      self[0], self[last] = self[last], self[0]
+      BinaryMinHeap.heapify_down(self, 0, last)
+    end
+
+    self.reverse!
   end
 end
