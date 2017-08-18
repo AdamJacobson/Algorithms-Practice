d87d0f7cbc92f601c553166669772e5cdfbad50b
adam <amjacobson0@gmail.com>
2017-08-18 09:45:12 -0700

    Partition complete

--- quicksort/adam_jacobson/lib/quick_sort.rb
+++ quicksort/adam_jacobson/lib/quick_sort.rb
@@ -8,8 +8,29 @@ class QuickSort
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
+
   end
 
+  # Rearranges the array into 2 parts
   def self.partition(array, start, length, &prc)
+    prc ||= Proc.new { |el1, el2| el1 <=> el2 }
+
+    pivot_idx = start
+
+    barrier = start
+    (start + 1).upto(start + length - 1) do |i|
+      if prc.call(array[start], array[i]) > 0
+
+        if barrier > start
+          array[i], array[barrier + 1] = array[barrier + 1], array[i]
+        end
+
+        barrier += 1
+      end
+    end
+
+    array[pivot_idx], array[barrier] = array[barrier], array[pivot_idx]
+
+    barrier
   end
 end
