8c3c2a09e24d41d0516f18d2d7ea70fce72abbdc
adam <amjacobson0@gmail.com>
2017-08-18 08:51:16 -0700

    Initial commit for quicksort

--- heap/adam_jacobson/lib/k_largest_elements.rb
+++ heap/adam_jacobson/lib/k_largest_elements.rb
@@ -2,5 +2,17 @@ require_relative 'heap'
 require_relative 'heap_sort'
 
 def k_largest_elements(array, k)
-  array.heap_sort!.drop(array.length - k)
+  result = BinaryMinHeap.new
+
+  k.times do
+    result.push(array.pop)
+  end
+
+  until array.empty?
+    result.push(array.pop)
+    result.extract
+  end
+  result.store
+
+  # array.heap_sort!.drop(array.length - k)
 end
--- heap/adam_jacobson/spec/k_largest_elements_spec.rb
+++ heap/adam_jacobson/spec/k_largest_elements_spec.rb
@@ -6,8 +6,8 @@ describe "#k_largest_elements" do
     arr1 = [0,1,2,3,4,5,6,7,8,9].shuffle
     result1 = arr1.sort.drop(6)
 
-    arr2 = (0..1000).to_a.sample(400)
-    result2 = arr2.sort.drop(390)
+    arr2 = (0..10000).to_a.sample(4000)
+    result2 = arr2.sort.drop(3990)
 
     expect(k_largest_elements(arr1, 4)).to match_array(result1)
     expect(k_largest_elements(arr2, 10)).to match_array(result2)
